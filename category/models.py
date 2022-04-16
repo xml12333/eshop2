eca4abfdb0a31cba0a114a33a73689f997f4285e
Nikolay Trafimenkov <xml12333@gmail.com>
2022-04-16 20:40:55 +0300

    first commit

---
+++ category/models.py
@@ -0,0 +1,20 @@
+from distutils.command.upload import upload
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50,unique=True, blank=True, null=True)
+    slug = models.SlugField(max_length=100,unique=True, blank=True, null=True)
+    description = models.TextField(max_length=250, blank=True, null=True)
+    cat_image = models.ImageField(upload_to='photos/categories/',blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
+
+    def __str__(self):
+        return self.category_name
